343e1efa0dd755390297632dec75089aa92ba724
Ariel Stern <arielstern09@gmail.com>
2025-07-08 17:00:12 -0400

    Added both Model and Controller for Product

---
+++ routes/product/productModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+   name: {
+    type: String,
+    required: true,
+  },
+   description: {
+    type: String,
+    default: ""
+  },
+   price:{
+    type: Number,
+    required: true
+  },
+
+   category: {
+    type: String,
+    required: true
+  },
+  stock: {
+    type: Number,
+    required: true
+  },
+  images: {
+    type: [String],
+    required: true
+  },
+  
+},
+
+  {
+    timestamps: true,
+  }
+
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
